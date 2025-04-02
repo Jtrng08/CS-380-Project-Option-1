6b52f5f02013e055d5c01c5979a746b555e6c2e1
valeriemay00 <vchavez8500@gmail.com>
2025-04-02 15:03:10 -0700

    Database connection for Traveler to book and search for flights

---
+++ FlightReservation/src/DatabaseConnection.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/FlightReservation";
+    private static final String USER = "root";
+    private static final String PASSWORD = "DELSCORCHO2@";
+
+    //method establishing and returning db connection
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to the Database.");
+        } catch (SQLException e) {
+            System.out.println("Database connection failed: " + e.getMessage());
+        }
+        return conn;
+    }
+        public static void main(String[] args) {
+        Connection tesConnection = connect();
+        if (tesConnection != null) {
+            System.out.println("Connection test successful!");
+        }
+    }
+}
